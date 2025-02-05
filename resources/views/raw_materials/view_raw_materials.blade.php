f5465aa7477b01f35d993471cc7489b2573ef763
Schultz Henry Boado <schultzxhenry@gmail.com>
2025-02-05 15:43:52 +0800

    done

--- resources/views/raw_materials/view_raw_materials.blade.php
+++ resources/views/raw_materials/view_raw_materials.blade.php
@@ -2,16 +2,16 @@
     <div class="modal-dialog">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title">View raw materials</h5>
+                <h5 class="modal-title">View raw materials - {{$raw_material->Name}}</h5>
             </div>
             <div class="modal-body">
                 @if(count($raw_material->product_material_compositions) > 0)
-                @foreach ($raw_material->product_material_compositions as $pmc)
-                    @if(!empty($pmc->products))
-                    {{optional($pmc->products)->code}} <br>
-                    <hr>
-                    @endif
-                @endforeach
+                    @foreach ($raw_material->product_material_compositions as $pmc)
+                        @if(!empty($pmc->products))
+                        {{optional($pmc->products)->code}} - {{$pmc->Percentage}}%<br>
+                        <hr>
+                        @endif
+                    @endforeach
                 @else
                 <p class="text-center">No data available.</p>
                 @endif
